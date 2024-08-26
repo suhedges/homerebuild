e4a56ea315186c6f08bc84b16c8090405f867cca
suhedges <160956126+suhedges@users.noreply.github.com>
2024-08-26 17:12:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 function filterCategories(category) {
     const allGroups = document.querySelectorAll('.category-group-container, .category-item');
+    const initialMessage = document.getElementById('initial-message');
+
+    if (category) {
+        initialMessage.style.display = 'none';
+    }
+
     allGroups.forEach(group => {
-        if (category === 'all' || group.dataset.category === category) {
+        if (group.dataset.category === category) {
             group.style.display = 'block';
         } else {
             group.style.display = 'none';
